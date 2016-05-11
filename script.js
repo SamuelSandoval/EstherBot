93fcfc7fe618b28526e11f423283df508b8938e8
Samuel Sandoval <Samuel.B.Sandoval@gmail.com>
2016-05-11 13:21:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ module.exports = new Script({
         }
     },
 
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name?'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Great! I'll call you ${name}`))
+                .then(() => 'finish');
+        }
+    },
+
     speak: {
         receive: (bot, message) => {
 
